d3732062b7f74081e5a8ba886bee96a1f20da67a
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-01-29 12:00:30 +0530

    ofboard

---
+++ server/routes/supplier.js
@@ -0,0 +1,21 @@
+// server/routes/supplier.js
+const express = require('express');
+const router = express.Router();
+const supplierController = require('../controllers/supplierController');
+
+// GET all suppliers
+router.get('/', supplierController.getAllSuppliers);
+
+// GET single supplier by ID
+router.get('/:id', supplierController.getSupplierById);
+
+// POST create new supplier
+router.post('/', supplierController.createSupplier);
+
+// PUT update supplier
+router.put('/:id', supplierController.updateSupplier);
+
+// DELETE supplier
+router.delete('/:id', supplierController.deleteSupplier);
+
+module.exports = router;
